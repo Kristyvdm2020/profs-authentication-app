1b35d7b79dba530b5ded8d172fd0bec50056d90a
Kristyvdm2020 <kristyvdm2020@gmail.com>
2022-12-19 10:50:24 -0500

    create login form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, Link} from 'react-router-dom';
-
+import Login from './Login'
 
 const App = ()=> {
   const [registerUsername, setRegisterUsername] = useState('');
@@ -33,33 +33,7 @@ const App = ()=> {
     exchangeTokenForUser();
   }, []);
 
-  const login = (ev) => {
-    ev.preventDefault();
-    console.log('login');
-    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/login', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: {
-          username: loginUsername,
-          password: loginPassword
-        }
-      })
-    })
-    .then(response => response.json())
-    .then(result => {
-      if(!result.success) {
-        console.log(result);
-        throw result.error;
-      }
-      exchangeTokenForUser();
-      })
-
-    .catch(err => console.log(err));
-    }
-
+  
   const register = (ev) => {
     ev.preventDefault();
     fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/register', {
@@ -112,17 +86,7 @@ const App = ()=> {
               onChange = {ev => setRegisterPassword(ev.target.value)}/>
             <button>Register</button>
           </form>
-          <form onSubmit = { login }>
-            <input 
-                placeholder='username' 
-                value={ loginUsername } 
-                onChange = {ev => setLoginUsername(ev.target.value)}/>
-            <input 
-              placeholder='password'
-              value={ loginPassword } 
-              onChange = {ev => setLoginPassword(ev.target.value)}/>
-            <button>Login</button>
-          </form>
+          <Login exchangeTokenForUser={ exchangeTokenForUser }/>
         </div>): null
       }
     </div>
